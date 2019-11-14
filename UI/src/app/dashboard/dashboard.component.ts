11d788bb24e74fe34b13833b7db2787f1922e0e8
Maneesh Divana <maneeshd77@gmail.com>
2019-11-14 16:43:42 -0600

    Project structure update

--- UI/src/app/dashboard/dashboard.component.ts
+++ UI/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
-import { Chart } from 'chart.js';
+import { Chart } from 'chart.js.js';
 import { AppComponent } from '../app.component';
 
 @Component({
